6379671cccd06e8dd1d8a3a8f2b734f34ae909e8
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2022-02-01 11:27:58 -0500

    exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
-
+  belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, presence: true, numericality: { more_than_or_equal_to: 40,
+              less_than_or_equal_to: 200 }
+  validates :store_id, numericality: { more_than_or_equal_to: 1 }
 end
